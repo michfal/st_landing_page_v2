06e6f8d7176ee9c90c26fb0b18a576faf262910a
Michał Fal <michfal86@gmail.com>
2021-08-08 01:57:03 +0200

    Setup component structure and basic styling. Added custom bullets list

--- src/components/sections/courseStructure/CourseStructure.component.jsx
+++ src/components/sections/courseStructure/CourseStructure.component.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
+import './courseStructure.scss'
+
 const CourseStructure = () => {
     return (
-        <section>
-            <h1>CourseStructure</h1>
+        <section className="structure">
+            <h3 className="structure__header">Całość składa się z:</h3>
+            <h4>36 spotkań po 60 minut</h4>
+            <h5>lub</h5>
+            <h4>18 spotkań po 120 minut</h4>
+            <ul className="structure__list">
+                <li>Ilość spotkań jest uzależniona od indywidualnych możliwości i umiejętności ucznia</li>
+                <li>Pierwsze spotkanie zawsze obejmuje poznanie wymagań i dostosowanie planu nauki</li>
+                <li>Możliwe konsultacje online</li>
+            </ul>
         </section>
     )
 }
