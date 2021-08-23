7ad22cbbe77aa3aa03495c08d89df87ee788513d
Michał Fal <michfal86@gmail.com>
2021-08-23 20:22:41 +0200

    Paragraph text component styling

--- src/components/ovalHeader/OvalHeader.component.jsx
+++ src/components/ovalHeader/OvalHeader.component.jsx
@@ -6,11 +6,11 @@ import './ovalHeader.scss'
 
 const OvalHeader = (props) => {
     return (
-        <>
+        <div>
             <h4 className={`oval_header txt_white ${props.bgColor}`} >
               {props.headerText}
             </h4>            
-        </>
+        </div>
     )
 }
 
--- src/components/paragraphText/ParagraphText.component.jsx
+++ src/components/paragraphText/ParagraphText.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
+import './paragraph_text.scss'
+
 const ParagraphText = ({paragraph}) => {
 
     const content = paragraph !== undefined
 
     return (
         <>
-            {content ? <p>{paragraph}</p> : null}
+            {content ? <p className="txt_purple--dark txt_purple--dark_regular paragraph_text">{paragraph}</p> : null}
         </>
     )
 }
