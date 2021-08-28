ab49a2b5961d9f95b9d35f4e87522504c1efbe6b
Michał Fal <michfal86@gmail.com>
2021-08-28 20:10:29 +0200

    added learning plan pointer

--- src/components/learningPlan/LearningPlan.component.jsx
+++ src/components/learningPlan/LearningPlan.component.jsx
@@ -3,6 +3,26 @@ import nextId from 'react-id-generator'
 
 import './learningPlan.scss'
 
+const planItem = (info) => {
+    console.log(info)
+    return (
+        <div className="lp_content">{
+            info.map((e, i) => {        
+                return (
+                    <div className="lp_content__item">
+                        <div className="lp_content__item_point">
+                            <h1 className="learning_plan_counter txt_white">{i+1}</h1>
+                        </div>
+                        <p className="txt_gray txt_gray--thematic_scope lp_content__item_text">{e}</p> 
+                    </div>
+                )
+            })
+        }</div>    
+    )
+    
+
+}
+
 const LearningPlan = ({info}) => {
     const list = info !== undefined
 
@@ -10,9 +30,9 @@ const LearningPlan = ({info}) => {
         <>  
             <div className="learning_plan">
                 <h1 className="txt_white learning_plan__header">Przykładowy program</h1>
-                <ul className="learning_plan__content">
-                    {list ? <ul>{info.map((e) => <li key={nextId()} className="txt_gray txt_gray--thematic_scope">{e}</li>)}</ul> : <ul></ul>}
-                </ul>
+                
+                    {/* {list ? <ol className="learning_plan__content">{info.map((e) => <li key={nextId()} className="txt_gray txt_gray--thematic_scope learning_plan__content_li">{e}</li>)}</ol> : null} */}
+                    {list ? planItem(info) : null}
             </div>
 
         </>
