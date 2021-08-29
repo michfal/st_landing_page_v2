1a566f6abae7b897d9a29872d92df0e3a2bede74
Michał Fal <michfal86@gmail.com>
2021-08-29 20:08:43 +0200

    learning plan point colors

--- src/components/learningPlan/LearningPlan.component.jsx
+++ src/components/learningPlan/LearningPlan.component.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import nextId from 'react-id-generator'
+import downArrowIcon from '../../images/arrow.svg'
 
 import './learningPlan.scss'
+
+const setColor = (i) => {
+    const number = i + 1;
+    if (number % 3 === 0) {
+        return 'point_green'
+    }
+    if ((number + 1) % 3 === 0) {
+        return 'point_violet'
+    }
+    
+}
 
 const planItem = (info) => {
     console.log(info)
     return (
         <div className="lp_content">{
-            info.map((e, i) => {        
+            info.map((e, i) => {
+                setColor(i)      
                 return (
-                    <div className="lp_content__item">
-                        <div className="lp_content__item_point">
+                    <div key={nextId()} className="lp_content__item">
+                        <div className={`lp_content__item_point ${setColor(i)}`}>
                             <h1 className="learning_plan_counter txt_white">{i+1}</h1>
                         </div>
                         <p className="txt_gray txt_gray--thematic_scope lp_content__item_text">{e}</p> 
@@ -19,8 +32,6 @@ const planItem = (info) => {
             })
         }</div>    
     )
-    
-
 }
 
 const LearningPlan = ({info}) => {
@@ -29,7 +40,8 @@ const LearningPlan = ({info}) => {
     return (
         <>  
             <div className="learning_plan">
-                <h1 className="txt_white learning_plan__header">Przykładowy program</h1>
+                {/* <img className="hero__scroll_icon" src={downArrowIcon} alt="down arrow icon" /> */}
+                <h1 className="txt_white txt_white--learning_plan learning_plan__header">Przykładowy program</h1>
                 
                     {/* {list ? <ol className="learning_plan__content">{info.map((e) => <li key={nextId()} className="txt_gray txt_gray--thematic_scope learning_plan__content_li">{e}</li>)}</ol> : null} */}
                     {list ? planItem(info) : null}
