d483dfd0035e3b9008201a340c12b5248aa0e650
Michał Fal <michfal86@gmail.com>
2021-09-13 19:27:15 +0200

    Added gatsby-image in hero section

--- src/components/sections/hero/Hero.component.jsx
+++ src/components/sections/hero/Hero.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 // import Fade from 'react-reveal/Fade'
 import { Link } from 'react-scroll'
+import { StaticImage } from "gatsby-plugin-image"
 
 import './hero.scss'
 
@@ -10,11 +11,25 @@ import downArrowIcon from '../../../images/arrow.svg'
 const Hero = () => {
     return(
         <section className="hero">
-            <img className="hero__logo" src={logo} alt="st logo" />
+            <StaticImage 
+                className="hero__logo" 
+                src="../../../images/st_logo.svg" 
+                alt="st logo"
+                placeholder="none"
+                quality="100"
+            />
+            {/* <img className="hero__logo" src={logo} alt="st logo" /> */}
             <Link activeClass="active" to="offer" spy={true} smooth={true} duration={1000}>
                 <img className="hero__scroll_icon" src={downArrowIcon} alt="down arrow icon" />
             </Link>
-            
+            <StaticImage 
+                className="hero__background" 
+                src="../../../images/main_bg.svg" 
+                alt="st logo"
+                placeholder="none"
+                // fit= "cover"
+                quality="100"
+            />
         </section>
     )
 };
