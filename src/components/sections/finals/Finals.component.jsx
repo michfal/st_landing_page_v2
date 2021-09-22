d27b89fd3d73d31a893c8ed23f0f558192d863d8
Michał Fal <michfal86@gmail.com>
2021-09-22 19:47:50 +0200

    Fixed learning plan responsivness

--- src/components/sections/finals/Finals.component.jsx
+++ src/components/sections/finals/Finals.component.jsx
@@ -12,14 +12,14 @@ const Finals = (props) => {
     //This doesn't work on scren resize has to be refactored with "useEffect" hook
     const width = window.screen.width;
     const {info, paragraph} = props
-    console.log((width >= 750) ? "big" : "small")
+
 
     return (
         <section id="finals" className="finals">
             <OvalHeader headerText="Egzamin Maturalny" bgColor="background-green"/>
             <ParagraphText paragraph={paragraph}/>
             <LearningPlan info={info} bgColor="background-green"/>
-            {(width >= 750) ? <Blob position={"blob_position-finals_tablet"} color={"blob_fill-light_green"}/> : null}
+            <Blob position={"blob_position-finals_tablet"} color={"blob_fill-light_green"}/>
         </section>
     )
 }
