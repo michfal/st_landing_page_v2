d61bc032382149e18efbaff396a62846328d8280
Michał Fal <michfal86@gmail.com>
2021-10-29 00:04:16 +0200

    Added animation to elementary egzam component

--- src/components/sections/elementaryEgzam/ElementaryEgzam.components.jsx
+++ src/components/sections/elementaryEgzam/ElementaryEgzam.components.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { StaticImage } from "gatsby-plugin-image"
+import Fade from 'react-reveal/Fade'
 
 import LearningPlan from '../../learningPlan/LearningPlan.component'
 import OvalHeader from '../../ovalHeader/OvalHeader.component'
@@ -14,20 +15,31 @@ const ElementaryEgzam = (props) => {
 
     return (
         <section id="elementary_egzam" className="elementary_egzam">
+            <StaticImage 
+                className="elementary_egzam__square_root_l" 
+                src="../../../images/square_root2.svg"
+                alt="" 
+                placeholder="none"
+                quality="100"
+            />
             <div className="elementary_egzam__content">
                 {(scrSize === 'large') ? 
+                    <Fade left wait={1000}>
                     <StaticImage 
                     className="elementary_egzam__image_owl" 
                     src="../../../images/owls_think.svg"
                     alt=""
                     placeholder="none"
                     quality="100"
-                />
+                    />
+                    </Fade>
                 : null}
-                <div className="elementary_egzam__text">
-                    <OvalHeader headerText="Egzamin 8-klasisty" bgColor="background-blue"/>
-                    <ParagraphText align={"paragraph_text-right"} paragraph={paragraph}/>
-                </div>
+                <Fade right>
+                    <div className="elementary_egzam__text">
+                        <OvalHeader headerText="Egzamin 8-klasisty" bgColor="background-blue"/>
+                        <ParagraphText align={"paragraph_text-right"} paragraph={paragraph}/>
+                    </div>
+                </Fade>
             </div>
             {(scrSize === 'large') ? <Blob position={"blob_position-elementary"} color={"blob_fill-light_green"}/>: null}
             <LearningPlan info={info} bgColor="background-blue" border="lp_content--border_blue"/>
