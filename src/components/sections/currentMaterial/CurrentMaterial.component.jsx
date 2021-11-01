4f57221e4f9cfec7cffdd18bd9f031033cd0685d
Michał Fal <michfal86@gmail.com>
2021-11-01 21:30:27 +0100

    Desktop layout for currentMaterial component

--- src/components/sections/currentMaterial/CurrentMaterial.component.jsx
+++ src/components/sections/currentMaterial/CurrentMaterial.component.jsx
@@ -1,18 +1,38 @@
 import React from 'react'
+import Fade from 'react-reveal/Fade'
+import { StaticImage } from "gatsby-plugin-image"
+
+import {SetFadeDuration} from '../../logic/setFadeDuration/SetFadeDuration'
 
 import OvalHeader from '../../ovalHeader/OvalHeader.component'
 import ParagraphText from '../../paragraphText/ParagraphText.component'
 
-import './CurrentMaterial.scss'
+import './currentMaterial.scss'
 
 const CurrentMaterial = (props) => {
 
-    const {paragraph} = props
-
+    const {paragraph, scrSize} = props
+    const dur = SetFadeDuration(scrSize)
     return (
         <section id="current_material" className="current_material">
-            <OvalHeader headerText="Opracowanie Bieżącego Materiału" bgColor="background-green"/>
-            <ParagraphText paragraph={paragraph}/>
+            <div className="current_material__content">
+                <div className="current_material__text">
+                    <OvalHeader headerText="Opracowanie Bieżącego Materiału" bgColor="background-green"/>
+                    <ParagraphText align={"paragraph_text-left"} paragraph={paragraph}/>
+                </div>
+                {(scrSize === 'large') ? 
+                    <Fade left duration={dur}>
+                        <StaticImage 
+                            className="image_owl-checklist" 
+                            src="../../../images/owls_checklist.svg"
+                            alt=""
+                            placeholder="none"
+                            quality="100"
+                        />
+                    </Fade>
+                : null}
+            </div>
+
         </section>
     )
 }
